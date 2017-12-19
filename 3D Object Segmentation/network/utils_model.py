c2ee89294b9a75dcf653fcbf5f8d1b4d826f558a
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2017-12-19 18:02:23 -0500

    add plot function

--- 3D Object Segmentation/network/utils_model.py
+++ 3D Object Segmentation/network/utils_model.py
@@ -7,13 +7,15 @@
 '''
   The file contains helper functions such as the viewPooling
 '''
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 import tensorflow as tf
 import numpy as np
 import pdb
+
+import matplotlib.pyplot as plt
+from PIL import Image
 
 
 '''
@@ -90,4 +92,71 @@ def L2Loss(y_pred, y_gt):
   loss = tf.reduce_mean(diff)
 
   return loss
+
+'''
+  plot curver between iteration times and loss or accuracy
+  - Input iteration: the iteration times of training
+  - Iuput loss: loss value during the training process
+  - Input accuracy: prediction accuracy during the training process
+'''
+def processPlot_acc_loss(iteration, loss, accuracy, test_loss, test_acc, title):
+  fig, (Ax0, Ax1) = plt.subplots(1, 2, figsize = (16, 8))
+
+  x = np.arange(0, iteration, 1)
+
+  Ax0.plot(x, loss)
+  # Ax0.text(0.5, 80, , fontsize=12)
+  Ax0.text(0.95, 0.01, 'The average test loss is {:.4f}'.format(test_loss),
+        verticalalignment='bottom', horizontalalignment='right', transform=Ax0.transAxes,
+        color='red', fontsize=15)
+
+  Ax0.set_title('Loss Value') 
+  Ax0.set_xlabel('iteration times')
+  Ax0.set_ylabel('loss')
+  Ax0.grid(True)
+  
+
+  Ax1.plot(x, accuracy)
+
+  Ax1.text(0.95, 0.01, 'The average test accuracy is {:.4f}%'.format(test_acc * 100),
+        verticalalignment='bottom', horizontalalignment='right', transform=Ax1.transAxes,
+        color='red', fontsize=15)
+  Ax1.set_title('Prediction Accuracy')
+  Ax1.set_xlabel('iteration times')
+  Ax1.set_ylabel('Accuracy')
+  Ax1.grid(True)
+
+  plt.suptitle(title, fontsize=16)
+  plt.show()
+
+
+'''
+  plot accuracy and loss curve wrt the iteration times
+'''
+def processPlot(ind_base):
+  test_res_cls = np.load('res/MVCNN{}_test_cls.npy'.format(ind_base))
+  test_res_reg = np.load('res/MVCNN{}_test_reg.npy'.format(ind_base))
+  test_res_depth = np.load('res/MVCNN{}_test_depth.npy'.format(ind_base)) 
+
+  train_cls_loss = np.load('res/MVCNN{}_train_cls_loss.npy'.format(ind_base))
+  train_cls_acc = np.load('res/MVCNN{}_train_cls_acc.npy'.format(ind_base))
+  train_reg_loss = np.load('res/MVCNN{}_train_reg_loss.npy'.format(ind_base))
+  train_reg_acc = np.load('res/MVCNN{}_train_reg_acc.npy'.format(ind_base))
+  train_depth_loss = np.load('res/MVCNN{}_train_depth_loss.npy'.format(ind_base))
+  train_depth_acc = np.load('res/MVCNN{}_train_depth_acc.npy'.format(ind_base))
+
+  total = train_cls_loss.shape[0]
+  title = 'Cls Branch: Training Loss and Accuracy Curves wrt the iteration (Include Test Result)'
+  processPlot_acc_loss(total, train_cls_loss, train_cls_acc, test_res_cls[0], test_res_cls[1], title)
+
+  title = 'Reg Branch: Training Loss and Accuracy Curves wrt the iteration (Include Test Result)'
+  processPlot_acc_loss(total, train_reg_loss, train_reg_acc, test_res_reg[0], test_res_reg[1], title)
+
+  title = 'Depth Branch: Training Loss and Accuracy Curves wrt the iteration (Include Test Result)'
+  processPlot_acc_loss(total, train_depth_loss, train_depth_acc, test_res_depth[0], test_res_depth[1], title)
+
+
+if __name__ == '__main__':
+  processPlot(2)
+
   

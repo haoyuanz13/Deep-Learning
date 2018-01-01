0fbcbfb31774483af55816639fb963777199f0f5
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2018-01-01 17:31:20 -0500

    train the celebA dataset

--- Generative Model/AE_VAE/VAE.py
+++ Generative Model/AE_VAE/VAE.py
@@ -24,14 +24,14 @@ flags.DEFINE_string("logdir", None, "Directory to save logs")
 flags.DEFINE_string("sampledir_cufs", 'vae_res/cufs_samples', "Directory to save cufs samples")
 flags.DEFINE_string("sampledir_celeba", 'vae_res/celeba_samples', "Directory to save celeba samples")
 flags.DEFINE_integer("total_num", 88, "The size of the dataset [88]")
-flags.DEFINE_integer("batch_size", 44, "The size of batch images [128]")
+flags.DEFINE_integer("batch_size", 100, "The size of batch images [128]")
 flags.DEFINE_integer("sample_size", 32, "The size of sample images [32]")
 flags.DEFINE_integer("out_channel", 1, "The channel number of data [1]")
 flags.DEFINE_integer("max_iterations", 10001, "The max iteration times [1e6]")
-flags.DEFINE_integer("interval_plot", 10, "The epoch interval to plot training loss [1000]")
+flags.DEFINE_integer("interval_plot", 1, "The epoch interval to plot training loss [1000]")
 flags.DEFINE_integer("interval_save", 100, "The epoch interval to save generative images [1000]")
 flags.DEFINE_boolean("debug", False, "True if debug mode")
-flags.DEFINE_boolean("addNoise", True, "True if adding noise to the raw data")
+flags.DEFINE_boolean("addNoise", False, "True if adding noise to the raw data")
 FLAGS = flags.FLAGS
 
 
@@ -87,7 +87,7 @@ def VAE_main(dataset_train, dataset_test, cufs=True):
   f_z_samples, _, _, _, _ = VAE(x_hat, x, reuse=True, is_train=False)  
   
   # VAE trainer 
-  lr_vae = .005  #.0002
+  lr_vae = .01  #.0002
   global_step = tf.Variable(0, name='global_step', trainable=False)
   vae_trainer = tf.train.AdamOptimizer(lr_vae, beta1=.5).minimize(comb_loss, global_step=global_step, var_list=var_vae)
 
@@ -107,8 +107,9 @@ def VAE_main(dataset_train, dataset_test, cufs=True):
   test_ori = merge(x_ipt_sample_target[:, :, :, 0], [int(math.sqrt(FLAGS.sample_size))] * 2)
   test_input = merge(x_ipt_sample_input[:, :, :, 0], [int(math.sqrt(FLAGS.sample_size))] * 2)
 
-  scipy.misc.imsave(FLAGS.sampledir_cufs + '/original_input.png', test_ori)
-  scipy.misc.imsave(FLAGS.sampledir_cufs + '/actual_input.png', test_input)
+  save_path = FLAGS.sampledir_cufs if cufs else FLAGS.sampledir_celeba
+  scipy.misc.imsave(save_path + '/original_input.png', test_ori)
+  scipy.misc.imsave(save_path + '/actual_input.png', test_input)
 
   # Training loop
   for epoch in range(2 if FLAGS.debug else int(FLAGS.max_iterations)): 
@@ -118,7 +119,6 @@ def VAE_main(dataset_train, dataset_test, cufs=True):
     data_epoch = dataset_train[arr[:], :, :, :]
 
     total_step = FLAGS.total_num / FLAGS.batch_size
-
 
     train_comb_loss, train_ML_loss, train_KL_div = 0, 0, 0
     for step in range(total_step):
@@ -185,8 +185,8 @@ def main(cufs=True):
   if cufs:
     dataset_train, _, dataset_test, _ = dataloader_cufs()
   else:
-    dataset_train = dataloader_celeba(3)
-    dataset_test = dataloader_celeba(1)
+    dataset_train = dataloader_celeba(3, twoSides=False)
+    dataset_test = dataloader_celeba(1, twoSides=False)
 
   # obtain output channel number (e.g. 1 for grayscale or 3 for rgb)
   FLAGS.total_num = dataset_train.shape[0]
@@ -196,6 +196,6 @@ def main(cufs=True):
 
 
 if __name__ == '__main__':
-  main()
+  main(cufs=False)
 
   # processPlot()

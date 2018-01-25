bb44f968927b805de9ae272552ee2aa48596a146
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2018-01-25 16:49:19 -0500

    change random seed value

--- Generative Model/cycleGAN_im2im/main_img2img.py
+++ Generative Model/cycleGAN_im2im/main_img2img.py
@@ -12,11 +12,12 @@ from model import *
 from cycleGAN import img2img
 
 
-slim = tf.contrib.slim
-tf.set_random_seed(1)
-np.random.seed(1)
-tf.logging.set_verbosity(tf.logging.INFO)
+# slim = tf.contrib.slim
+# tf.set_random_seed(1)
+# np.random.seed(1)
+# tf.logging.set_verbosity(tf.logging.INFO)
 
+tf.set_random_seed(19)
 
 ################
 # Define args  #
@@ -29,7 +30,7 @@ parser.add_argument('--test_dir', dest='test_dir', default='cycleGAN_res/test', 
 parser.add_argument('--sample_dir', dest='sample_dir', default='cycleGAN_res/samples', help='samples are saved here')
 parser.add_argument('--curve_dir', dest='curve_dir', default='cycleGAN_res/curves', help='loss curves are saved here')
 parser.add_argument('--log_dir', dest='log_dir', default='cycleGAN_res/logs', help='graphs are saved here')
-parser.add_argument('--train_size', dest='train_size', type=int, default=300, help='max # images used to train')
+parser.add_argument('--train_size', dest='train_size', type=int, default=230, help='max # images used to train')
 parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')
 parser.add_argument('--sample_size', dest='sample_size', type=int, default=1, help='# images in sample')
 parser.add_argument('--image_height', dest='image_height', type=int, default=256, help='standard image height')
@@ -41,7 +42,8 @@ parser.add_argument('--interval_sample', dest='interval_sample', type=int, defau
 parser.add_argument('--interval_save', dest='interval_save', type=int, default=100, help='# of epoch intervals to save model')
 parser.add_argument('--dim_first_modelG', dest='dim_first_modelG', type=int, default=64, help='# of gen filters in first conv layer')
 parser.add_argument('--dim_first_modelD', dest='dim_first_modelD', type=int, default=64, help='# of discri filters in first conv layer')
-parser.add_argument('--L1_loss_weight', dest='L1_loss_weight', type=float, default=100.0, help='weight on L1 term in objective')
+parser.add_argument('--L1_loss_weight', dest='L1_loss_weight', type=float, default=10.0, help='weight on L1 term in objective')
+parser.add_argument('--epoch_step', dest='epoch_step', type=int, default=500, help='# of epoch to decay lr')
 parser.add_argument('--lr_modelG', dest='lr_modelG', type=float, default=0.0002, help='initial learning rate for adam [model G]')
 parser.add_argument('--lr_modelD', dest='lr_modelD', type=float, default=0.0002, help='initial learning rate for adam [model D]')
 parser.add_argument('--n_critic_G', dest='n_critic_G', type=int, default=1, help='# of model G training in each step')
@@ -52,8 +54,8 @@ parser.add_argument('--addNoise', dest='addNoise', type=bool, default=False, hel
 parser.add_argument('--debug', dest='debug', type=bool, default=False, help='if debug the model instead of training')
 parser.add_argument('--curveShow', dest='curveShow', type=bool, default=False, help='if show loss curves')
 parser.add_argument('--phase', dest='phase', default='train', help='train, test')
-parser.add_argument('--randomSample', dest='randomSample', type=bool, default=True, help='if sample randomly')
-parser.add_argument('--concatSamples', dest='concatSamples', type=bool, default=True, help='if generate concatenated samples')
+parser.add_argument('--randomSample', dest='randomSample', type=bool, default=False, help='if sample randomly')
+parser.add_argument('--concatSamples', dest='concatSamples', type=bool, default=False, help='if generate concatenated samples')
 parser.add_argument('--generator_resnet', dest='generator_resnet', type=bool, default=True, help='if use generator_resnet structure')
 parser.add_argument('--loglikelihood_loss', dest='loglikelihood_loss', type=bool, default=False, help='if use log likelihood loss')
 parser.add_argument('--imgPool_max', dest='imgPool_max', type=int, default=50, help='max # of images in the image pool')
@@ -76,7 +78,7 @@ args = parser.parse_args()
 ########
 # Main #
 ########
-def main():
+def main(_):
   # check necessary directories
   print ("\n=====>> Checking necessary directories ..........")
   if not os.path.exists(args.res_dir):
@@ -98,8 +100,14 @@ def main():
 
   # build model
   print ("=====>> Initializing cycleGAN_img2img model ..........")
+  # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)    
+  # with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
 
-  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.67)    
+  # tfconfig = tf.ConfigProto(allow_soft_placement=True)
+  # tfconfig.gpu_options.allow_growth = True  
+  # with tf.Session(config=tfconfig) as sess:
+
+  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.899)    
   with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     print ("\n========== " + args.dataset_name.upper() + " dataset (Raw images) ==========")
     # args.max_iteration = 200      
@@ -126,6 +134,10 @@ def main():
 
 
 if __name__ == '__main__':
-  main()
+  # main()
+  tf.app.run()
+
+
+
 
 
